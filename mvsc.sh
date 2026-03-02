c5b41915e1aa4876dc28d6e6227e572b164bbcf6
Nabil A <nazam9519@gmail.com>
2026-03-02 17:30:42 -0500

    removed some files and mofied others

--- mvsc.sh
+++ mvsc.sh
@@ -5,5 +5,8 @@ export XWIKI_VERSION=17.10.3
 export JIRA_VERSION=11.1.2
 export CONF_VERSION=1.0.38
 export XWIKI_PRO_VERSION=13.13.4
+export DRAWIO_VERSION=1.22.11
 
-mvn clean install -U    
+./gen_pom.py
+mvn clean install -U
+#mvn clean package -U && mvn install -U
